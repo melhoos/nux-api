d81801b8ea50d12121686ab286ff3460f3f6bcf3
melhoos <mmelhoos@gmail.com>
2020-03-29 17:59:29 +0200

    added cors rules and updated ingress

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // https://medium.com/@bbkashikar/create-rest-api-with-node-js-and-sql-server-as-database-550be4471fb6
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 var port = process.env.port || 60
+var allowedOrigins = ['http://localhost:3000', 'http://localhost:8080', 'http://nux.e74c370edf4345d39982.westeurope.aksapp.io'];
+
+app.use(cors({
+    origin: function(origin, callback) {
+        console.log("---- origin: ", origin)
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                        'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.listen(port, () => {
     console.log(`Hi, port ${port} is running`);
